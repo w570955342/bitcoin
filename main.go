d8b39a1bb223f1d5696cf4d359e093134b6df108
w570955342 <42182202+w570955342@users.noreply.github.com>
2018-10-14 21:43:52 +0800

    '04_为BlockChain绑定AddBlock方法'

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package main
+package bitcoin
 
 import (
 	"crypto/sha256"
@@ -58,17 +58,26 @@ func GenesisBlock() *Block{
 }
 
 //6. 添加区块
+func (bc *BlockChain)AddBlock(data string)  {
+	lastBlock:=bc.blocks[len(bc.blocks)-1]
+	prevHash:=lastBlock.Hash
+
+	block:=NewBlock(data,prevHash)
+	bc.blocks=append(bc.blocks,block)
+}
 //7. 重构代码
 
 func main() {
 	blockChain:=NewBlockChain()
+	blockChain.AddBlock("这位大哥用两万个比特币买了一张pizza！")
+	blockChain.AddBlock("比特币暴跌，挖矿市场一片鬼哭狼嚎！")
 
 	for idx,block:=range blockChain.blocks{
 		fmt.Printf("================================= 当前区块高度：%d =================================\n",idx)
-		fmt.Printf("前区块哈希值： %x\n", block.PrevHash)
-		fmt.Printf("当前区块哈希值： %x\n", block.Hash)
-		fmt.Printf("区块数据 :%s\n", block.Data)
-		fmt.Println("成功生成新的区块！")
+		fmt.Printf("前区块哈希值： 	%x\n", block.PrevHash)
+		fmt.Printf("当前区块哈希值：	%x\n", block.Hash)
+		fmt.Printf("区块数据 :	%s\n", block.Data)
+		//fmt.Println("成功生成新的区块！")
 	}
 
 }
