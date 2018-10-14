7639e328947c59463ef6c81a6e5e58c201882a74
w570955342 <42182202+w570955342@users.noreply.github.com>
2018-10-14 23:42:25 +0800

    '06-添加完整的区块字段并引用'

--- block.go
+++ block.go
@@ -1,21 +1,45 @@
 package main
 
-import "crypto/sha256"
+import (
+	"crypto/sha256"
+	"time"
+)
 
 //1. 定义区块结构
 type Block struct {
-	//1. 前区块哈希
+	//1. 版本号
+	Version uint64
+	//2. 前区块哈希
 	PrevHash []byte
-	//2. 当前区块哈希
+	//3. 梅克尔根
+	MerkelRoot []byte
+	//4. 时间戳
+	TimeStamp uint64
+	//5. 难度值
+	Difficulty uint64
+	//6. 随机数
+	Nonce uint64
+
+	//a. 当前区块哈希,正常比特币区块中没有当前区块的哈希！
 	Hash []byte
-	//3. 交易数据
+	//b. 交易数据
 	Data []byte
+}
+
+func Uint64ToByte(num uint64) []byte {
+	//TODO
+	return []byte{}
 }
 
 //2. 创建区块
 func NewBlock(data string, prevBlockHash []byte) *Block {
 	block := Block{
+		Version:00,
 		PrevHash: prevBlockHash,
+		MerkelRoot:[]byte{},
+		TimeStamp:uint64(time.Now().Unix()),
+		Difficulty:0,
+		Nonce:0,
 		Hash:     []byte{}, //先填空，后面再计算 //TODO
 		Data:     []byte(data),
 	}
@@ -26,8 +50,15 @@ func NewBlock(data string, prevBlockHash []byte) *Block {
 
 //3. 为Block绑定方法生成哈希
 func (block *Block) SetHash() {
+	var blockInfo []byte
 	//1. 拼接数据
-	blockInfo := append(block.PrevHash, block.Data...)
+	blockInfo = append(blockInfo, Uint64ToByte(block.Version)...)
+	blockInfo = append(blockInfo, block.PrevHash...)
+	blockInfo = append(blockInfo, block.MerkelRoot...)
+	blockInfo = append(blockInfo, Uint64ToByte(block.TimeStamp)...)
+	blockInfo = append(blockInfo, Uint64ToByte(block.Difficulty)...)
+	blockInfo = append(blockInfo, Uint64ToByte(block.Nonce)...)
+	blockInfo = append(block.PrevHash, block.Data...)
 
 	//2. sha256
 	hash := sha256.Sum256(blockInfo)
