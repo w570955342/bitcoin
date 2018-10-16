452b6e14cdb762be45437c33901025940713fc17
w570955342 <42182202+w570955342@users.noreply.github.com>
2018-10-16 21:45:06 +0800

    15_创建Next方法、完成区块链遍历

--- blockchainiterator.go
+++ blockchainiterator.go
@@ -22,4 +22,28 @@ func (bc *BlockChain) NewIterator() *BlockChainIterator {
 	}
 }
 
+//迭代器是属于区块链的
+//Next方式是属于迭代器的
+//1. 返回当前的区块
+//2. 指针前移
+func (it *BlockChainIterator) Next() *Block {
+	var block Block
+	it.Db.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte(BlockBucket))
+		if bucket == nil {
+			log.Panic("迭代器遍历时bucket不应该为空，请检查!")
+		}
+
+		blockTmp := bucket.Get(it.CurrentHashPtr)
+		//解码动作
+		block = Deserialize(blockTmp)
+		//游标哈希左移
+		it.CurrentHashPtr = block.PrevHash
+
+		return nil
+	})
+
+	return &block
+}
+
 
