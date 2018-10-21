b19ecb2668b042975d1fb20136777231e15f11ff
w570955342 <42182202+w570955342@users.noreply.github.com>
2018-10-21 08:51:03 +0800

    34_为Wallet绑定loadFile方法，加载本地wallet.dat文件到内存

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ package main
 func main() {
 	blockChain:=NewBlockChain("张三")
 	cli := CLI{blockChain}
+	defer blockChain.Db.Close()
 	cli.Run()
 	//blockChain.AddBlock("这位大哥用两万个比特币买了一张pizza！")
 	//blockChain.AddBlock("比特币暴跌，挖矿市场一片鬼哭狼嚎！")
--- wallet.go
+++ wallet.go
@@ -11,14 +11,15 @@ import (
 //定一个 Wallet结构，它保存所有的Key以及它的地址
 type Wallet struct {
 	//map[地址]秘钥
-	WalletsMap map[string]*Key
+	WalletMap map[string]*Key
 }
 
 //生成钱包
-func NewWallet() *Wallet {
+func NewWallet() *Wallet{
 
 	var wallet Wallet
-	wallet.WalletsMap = make(map[string]*Key)
+	//wallet.WalletsMap = make(map[string]*Key)
+	wallet.loadFile()
 	return &wallet
 }
 
@@ -26,7 +27,7 @@ func (wallet *Wallet)CreateWallet()string  {
 	key:=NewKey()
 	address:=key.NewAddress()
 
-	wallet.WalletsMap[address]=key
+	wallet.WalletMap[address]=key
 	wallet.saveToFile()
 	return address
 }
@@ -45,4 +46,27 @@ func (wallet *Wallet) saveToFile() {
 	ioutil.WriteFile("wallet.dat", buffer.Bytes(), 0600)
 }
 
-//读取文件方法，把所有的key读出来
+//读取文件方法，把所有的key读出来,存在内存中
+func (wallet *Wallet)loadFile()  {
+	//读取内容
+	content, err := ioutil.ReadFile("wallet.dat")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	//解码
+	//panic: gob: type not registered for interface: elliptic.p256Curve
+	gob.Register(elliptic.P256())
+	decoder := gob.NewDecoder(bytes.NewReader(content))
+
+	var walletLocal Wallet
+
+	err = decoder.Decode(&walletLocal)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	//wallet = &walletLocal
+	//对于结构来说，里面有map的，要指定赋值，不要再最外层直接赋值
+	wallet.WalletMap = walletLocal.WalletMap
+}
